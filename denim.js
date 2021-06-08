6f568e6c4fe5c2ece563860cf18aab0847840081
hanneton <blush3d@ya.ru>
2021-06-08 23:23:02 +0300

    tabs are added, without animation

--- denim.js
+++ denim.js
@@ -1,6 +1,28 @@
-window.onload = function() {
-    let redCross = document.querySelector('.dropdown-menu-cross');
-    redCross.onclick = () => {
-        document.querySelector('.dropdown-menu').style.display = 'none'
+window.addEventListener('DOMContentLoaded', () => {
+    
+    const pressQuoteNPic = document.querySelectorAll('.press-quote-n-pic'),
+          pressCarouselItems = document.querySelectorAll('.press-carousel-item');
+
+    function showPressCard(index=0) {
+        pressQuoteNPic[index].style.display = 'block';
+        pressCarouselItems[index].classList.add('item-active');
     }
-}
+    showPressCard();
+
+    function hidePressCards() {
+        pressQuoteNPic.forEach(item => {
+            item.style.display = 'none';
+        })
+        pressCarouselItems.forEach(item => {
+            item.classList.remove('item-active');
+        })
+    }
+
+    pressCarouselItems.forEach((item, index) => {
+        item.addEventListener('click', (event) => {
+            event.preventDefault();
+            hidePressCards();
+            showPressCard(index);
+        })
+    })
+})
